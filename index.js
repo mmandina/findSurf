712623949ffd84f7eb08e248e7fc5890e0b89fce
Maxwell Mandina MAC <maxwell.mandina@gmail.com>
2022-10-12 10:55:10 -0400

    cleanup

--- index.js
+++ index.js
@@ -2,25 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
-//sudo systemctl start mongod
-//sudo mongod --dbpath /home/max/mongo/data
-//tests
 const Surfspotmodel = require("./models/ScraperSpot.js");
 const methodOverride = require("method-override");
 const Surfspot = Surfspotmodel.Surfspot;
-const surfSpotDescriptors=Surfspotmodel.surfSpotDescriptors
-const asyncWrap = require("./utilities/asyncWrap")
-const ExpressError= require("./utilities/ExpressError")
-const URI=require("./connectString").connectString
-const mapsApiKey=require('./mapsAPIKey').mapsAPIKey
+const surfSpotDescriptors = Surfspotmodel.surfSpotDescriptors;
+const asyncWrap = require("./utilities/asyncWrap");
+const ExpressError = require("./utilities/ExpressError");
+const URI = require("./connectString").connectString;
+const mapsApiKey = require("./mapsAPIKey").mapsAPIKey;
 
-
-//const URI=connectURI.connectString
 mongoose
   .connect(URI, {
     // dbName: "findSurf",
-  usenewUrlParser: true,
- 
+    usenewUrlParser: true,
+
     useUnifiedTopology: true,
   })
   .then(() => {
@@ -36,89 +31,141 @@ app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async(req, res) => {
-  const test = await Surfspot.findOne()
-  //console.log(test.location.country);
+app.get("/", async (req, res) => {
+  const test = await Surfspot.findOne();
   res.redirect("/surfspots");
 });
-app.post("/surfspots/search", asyncWrap(async (req, res) => {
-  const surfspots = await Surfspot.find({
-    $or: [{
-      "spotName":
-        { $regex: new RegExp(/*"^" + */req.body.searchText.toLowerCase(), "i") }
-    },
-      {
-      "location.country":
-        { $regex: new RegExp(/*"^" + */req.body.searchText.toLowerCase(), "i") }
-      },
-      {
-        "location.Subzone1":
-          { $regex: new RegExp(/*"^" + */req.body.searchText.toLowerCase(), "i") }
-      },
-      {
-        "location.Subzone2":
-          { $regex: new RegExp(/*"^" + */req.body.searchText.toLowerCase(), "i") }
-      },
-      {
-        "location.subzone3":
-          { $regex: new RegExp(/*"^" + */req.body.searchText.toLowerCase(), "i") }
-      },
-      {
-        "location.subzone4":
-          { $regex: new RegExp(/*"^" + */req.body.searchText.toLowerCase(), "i") }
-      }
-    ]
-  });
-  res.render("surfspots/searchResult", { surfspots, title:"Results"})
-  //res.send(req.body);
-}));
+app.post(
+  "/surfspots/search",
+  asyncWrap(async (req, res) => {
+    const surfspots = await Surfspot.find({
+      $or: [
+        {
+          spotName: {
+            $regex: new RegExp(
+              /*"^" + */ req.body.searchText.toLowerCase(),
+              "i"
+            ),
+          },
+        },
+        {
+          "location.country": {
+            $regex: new RegExp(
+              /*"^" + */ req.body.searchText.toLowerCase(),
+              "i"
+            ),
+          },
+        },
+        {
+          "location.Subzone1": {
+            $regex: new RegExp(
+              /*"^" + */ req.body.searchText.toLowerCase(),
+              "i"
+            ),
+          },
+        },
+        {
+          "location.Subzone2": {
+            $regex: new RegExp(
+              /*"^" + */ req.body.searchText.toLowerCase(),
+              "i"
+            ),
+          },
+        },
+        {
+          "location.subzone3": {
+            $regex: new RegExp(
+              /*"^" + */ req.body.searchText.toLowerCase(),
+              "i"
+            ),
+          },
+        },
+        {
+          "location.subzone4": {
+            $regex: new RegExp(
+              /*"^" + */ req.body.searchText.toLowerCase(),
+              "i"
+            ),
+          },
+        },
+      ],
+    });
+    res.render("surfspots/searchResult", { surfspots, title: "Results" });
+    //res.send(req.body);
+  })
+);
 
-app.get("/surfspots", asyncWrap(async (req, res) => {
-  const surfspots = await Surfspot.find({}).limit(11);
-  res.render("surfspots/index", { surfspots, title: "Surfspot Index" });
-}));
+app.get(
+  "/surfspots",
+  asyncWrap(async (req, res) => {
+    const surfspots = await Surfspot.find({}).limit(11);
+    res.render("surfspots/index", { surfspots, title: "Surfspot Index" });
+  })
+);
 app.get("/surfspots/new", (req, res) => {
-  res.render("surfspots/new", { surfSpotDescriptors, title:"Submit New Surfspot" });
-});
-app.get("/surfspots/edit/:id", asyncWrap(async (req, res) => {
-  const spotId = req.params.id;
-  const spot = await Surfspot.findById(spotId);
-  res.render("surfspots/edit", {
-    spot,
+  res.render("surfspots/new", {
     surfSpotDescriptors,
-    title: `Edit Spot: ${spot.title}`,
+    title: "Submit New Surfspot",
   });
-}));
+});
+app.get(
+  "/surfspots/edit/:id",
+  asyncWrap(async (req, res) => {
+    const spotId = req.params.id;
+    const spot = await Surfspot.findById(spotId);
+    res.render("surfspots/edit", {
+      spot,
+      surfSpotDescriptors,
+      title: `Edit Spot: ${spot.title}`,
+    });
+  })
+);
 
-app.put("/surfspots/edit/:id/", asyncWrap(async (req, res) => {
-  const spotId = req.params.id;
-  const updatedSpot = await Surfspot.findByIdAndUpdate(
-    spotId,
-    req.body.surfspot
-  );
-  res.redirect(`/surfspots/detail/${updatedSpot._id}`);
-}));
-app.get("/surfspots/detail/:id", asyncWrap(async (req, res) => {
-  const spotId = req.params.id;
-  const spot = await Surfspot.findById(spotId);
-  res.render("surfspots/detailMap", { spot, title: `${spot.title}`,apiKey:mapsApiKey });
-}));
-app.delete("/surfspots/:id", asyncWrap(async (req, res) => {
-  const spotId = req.params.id;
-  await Surfspot.findByIdAndDelete(spotId);
-  res.redirect("/surfspots/");
-}));
-app.post("/surfspots/", asyncWrap(async (req, res) => {
-  const newSpot = new Surfspot(req.body.surfspot);
-  await newSpot.save();
-  res.redirect("/surfspots");
-}));
-app.all('*', (req, res, next) => {
-next(new ExpressError('PAGE NOT FOUND',404))
-})
-app.use((err,req, res,next) => {
-  const { status = 500, message = 'ERROR' } = err;
-  res.status(status).send(message)
+app.put(
+  "/surfspots/edit/:id/",
+  asyncWrap(async (req, res) => {
+    const spotId = req.params.id;
+    const updatedSpot = await Surfspot.findByIdAndUpdate(
+      spotId,
+      req.body.surfspot
+    );
+    res.redirect(`/surfspots/detail/${updatedSpot._id}`);
+  })
+);
+app.get(
+  "/surfspots/detail/:id",
+  asyncWrap(async (req, res) => {
+    const spotId = req.params.id;
+    const spot = await Surfspot.findById(spotId);
+    res.render("surfspots/detailMap", {
+      spot,
+      title: `${spot.title}`,
+      apiKey: mapsApiKey,
+    });
+  })
+);
+app.delete(
+  "/surfspots/:id",
+  asyncWrap(async (req, res) => {
+    const spotId = req.params.id;
+    await Surfspot.findByIdAndDelete(spotId);
+    res.redirect("/surfspots/");
+  })
+);
+app.post(
+  "/surfspots/",
+  asyncWrap(async (req, res) => {
+    const newSpot = new Surfspot(req.body.surfspot);
+    await newSpot.save();
+    res.redirect("/surfspots");
+  })
+);
+app.all("*", (req, res, next) => {
+  next(new ExpressError("PAGE NOT FOUND", 404));
+});
+app.use((err, req, res, next) => {
+  const { status = 500, message = "ERROR" } = err;
+  res.status(status).send(message);
 });
 app.listen(3000, () => {
   console.log("serving port 3000");
--- models/ScraperSpot.js
+++ models/ScraperSpot.js
@@ -1,45 +1,47 @@
 const mongoose = require("mongoose");
-const CoordinatesConv = require('coordinate-parser');
+const CoordinatesConv = require("coordinate-parser");
 const Schema = mongoose.Schema;
 //https://www.wannasurf.com/spot/North_America/USA/North_East/New_Jersey/36th_Street/index.html
-//const mongoose_fuzzy_searching = require("@imranbarbhuiya/mongoose-fuzzy-searching");
 
 SurfspotSchema = new Schema({
-  spotName: { //36th Street
+  spotName: {
+    //36th Street
     type: String,
     required: true,
   },
-  location: { //USA, North East, New Jersey
+  location: {
+    //USA, North East, New Jersey
     country: String, //USA
     Subzone1: String, //North East
     Subzone2: String, //New Jersey
     subzone3: String, //''
-    subzone4:String //''
+    subzone4: String, //''
   },
-  
-  hasCoordinates: { //True
+
+  hasCoordinates: {
+    //True
     type: Boolean,
-    required:true,
+    required: true,
   },
-  coordinates:{
-    Lat: String,// 39° 9.431' N
-    Lon:String//74° 41.154' W
+  coordinates: {
+    Lat: String, // 39° 9.431' N
+    Lon: String, //74° 41.154' W
   },
   access: {
     description: String, //easy access. Park at any of the meters lining Landis Avenue or on the off str
     distance: String, //In the city
-    walk:String, //Instant Access
+    walk: String, //Instant Access
     easyToFind: String, //Easy to Find
     publicAccess: String, //Public Access
-    specialAccess:String, //Don't know
+    specialAccess: String, //Don't know
   },
-    surfSpotQuality: {
-      waveQuality: String, //Normal
-      experience: String, //All Surfers
-      frequency:String, //Very Consistent
-    },
- waveChars: {
-  type: { type: String }, //Sand-bar
+  surfSpotQuality: {
+    waveQuality: String, //Normal
+    experience: String, //All Surfers
+    frequency: String, //Very Consistent
+  },
+  waveChars: {
+    type: { type: String }, //Sand-bar
     direction: String, //right and left
     bottom: String, //Sandy
     power: String, //Ordinary, fun
@@ -47,7 +49,7 @@ SurfspotSchema = new Schema({
     goodlength: String, //long (150-300m)
   },
   enviroChars: {
-    swellDir: String,//North, South, SouthEast, East, NorthEast
+    swellDir: String, //North, South, SouthEast, East, NorthEast
     windDir: String, //NorthWest, West
     swellSize: String, //Starts working at Less than 1m / 3ft and holds up to 4m+ / 12ft
     tidePos: String, //Mid and high tide
@@ -57,49 +59,41 @@ SurfspotSchema = new Schema({
     weekCrowd: String, //few surfers
     weekendCrowd: String, //crowded
     webcamUrl: String, //''
-    
   },
-  dangers: String,//Rips, Undertow, Rocks
+  dangers: String, //Rips, Undertow, Rocks
   info: {
-    description: String, //Consistent spot all year. During summer and hurricane season, the 
+    description: String, //Consistent spot all year. During summer and hurricane season, the
     atmosphere: String, //This is a great spot especially if you are y
-    general:String, //''
+    general: String, //''
   },
   image: String,
-  
 });
-SurfspotSchema.methods.locationStringify= function(){
+SurfspotSchema.methods.locationStringify = function () {
   let locationArray = Object.values(this.location);
-  let locationString = '';
+  let locationString = "";
   for (let i = 0; i < locationArray.length; i++) {
-    let value = locationArray[i]
+    let value = locationArray[i];
     if (locationArray[i]) {
-      locationString += value
+      locationString += value;
       if (locationArray[i + 1]) {
-        locationString+=', '
+        locationString += ", ";
       }
     } else {
-      locationString+=''
+      locationString += "";
     }
-    
-    
   }
-  return locationString
-}
-SurfspotSchema.methods.coordinateConverter= function(){
-  let lat=this.coordinates.Lat
-  let lon=this.coordinates.Lon
+  return locationString;
+};
+SurfspotSchema.methods.coordinateConverter = function () {
+  let lat = this.coordinates.Lat;
+  let lon = this.coordinates.Lon;
   let converted;
 
   converted = new CoordinatesConv(`${lat}, ${lon}`);
   let newLat = converted.getLatitude();
   let newLong = converted.getLongitude();
 
-  return [newLat,newLong]
-}
+  return [newLat, newLong];
+};
 
-
-//SurfspotSchema.plugin(mongoose_fuzzy_searching.sort, { fields: ['spotName', 'location'] })
-//SurfspotSchema.plugin(mongoose_fuzzy_searching.confidenceScore, { fields: ['spotName', 'location'] })
-//SurfspotSchema.plugin(mongoose_fuzzy_searching.default, { fields: ['spotName'] });
 exports.Surfspot = mongoose.model("surfspot", SurfspotSchema);
