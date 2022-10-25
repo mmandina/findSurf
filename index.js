5ba9f2f49af63e235c799e5e9f851e800cdf49ef
Maxwell Mandina MAC <maxwell.mandina@gmail.com>
2022-10-25 16:17:52 -0400

    removed secure cookies

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const sessionConfig = {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true,
-    secure: true,
+    // secure: true,
   },
 };
 app.use(session(sessionConfig));
