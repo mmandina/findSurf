3d1c0eaeafc07a1d9706835f45a4d46d806e6d0a
Maxwell Mandina MAC <maxwell.mandina@gmail.com>
2022-10-28 15:08:41 -0400

    updated documentation

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,9 +1,12 @@
 module.exports.isAdmin = (req, res, next) => {
+  //checks if the request user data has isAdmin set to true
   if (!req.user.isAdmin) {
     req.session.returnTo = req.originalUrl.split("/");
+    //checks if redirected from edit page
     if (req.session.returnTo[2] === "edit") {
       req.session.returnTo = req.session.returnTo[3];
     } else {
+      //If not from edit page, then from delete page (only two paths currently requiring admin priviliges)
       req.session.returnTo = req.session.returnTo[2].split("?")[0];
     }
     if (req.session.returnTo) {
