34bb9671502cb03634c62f32d4c63a359376c26a
Maxwell Mandina MAC <maxwell.mandina@gmail.com>
2022-10-28 15:40:25 -0400

    keystrings

---
+++ mapsAPIKey.js
@@ -0,0 +1,5 @@
+//Google Maps API key
+const APIcall =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDyWCoO_MCZNqbpIGB8PmlQo_GckQFLa5k&callback=initMap&v=weekly";
+
+exports.mapsAPIKey = APIcall;
